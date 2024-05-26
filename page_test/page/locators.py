92dca9a29683dc1dcc7f228a24531deae775a277
Kristina P <badmovochka@gmail.com>
2024-05-26 17:41:11 +0300

    Main page header and footer tests added

--- page_test/page/locators.py
+++ page_test/page/locators.py
@@ -1,19 +1,51 @@
 import os
 
-from Maksim_Tsybulka.class_work_9_f.page.base_page import WebPage
-from Maksim_Tsybulka.class_work_9_f.page.elements import WebElement
+from page_test.page.base_page import WebPage
+from page_test.page.elements import WebElement
 
 
 class MainPage(WebPage):
 
     def __init__(self, web_driver, url=''):
         if not url:
-            url = os.getenv("MAIN_URL") or 'https://hoster.by'
+            url = os.getenv("MAIN_URL") or 'https://habr.com/ru/feed/'
 
         super().__init__(web_driver, url)
 
-    btn_headers_domain = WebElement(xpath='//div[@id="menu-item"]//span[contains(text(),"Домены")]')
-    btn_headers_hosting = WebElement(xpath='//div[@id="menu-item"]//span[contains(text(),"Хостинг")]')
-    btn_headers_cloud = WebElement(xpath='//div[@id="menu-item"]//span[contains(text(),"Облако")]')
-    btn_headers_mail = WebElement(xpath='//div[@id="menu-item"]//span[contains(text(),"Почта")]')
+    #header buttons
+    btn_headers_1_my_feed = WebElement(xpath='//*[@class = "tm-main-menu__item tm-main-menu__item_active" and text()="Моя лента"]')
+    btn_headers_2_all_streams = WebElement(xpath='//*[@class = "tm-main-menu__item" and text()="Все потоки"]')
+    btn_headers_3_development = WebElement(xpath='//*[@class = "tm-main-menu__item" and text()="Разработка"]')
+    btn_headers_4_admin = WebElement(xpath='//*[@class = "tm-main-menu__item" and text()="Администрирование"]')
+    btn_headers_5_design = WebElement(xpath='//*[@class = "tm-main-menu__item" and text()="Дизайн"]')
+    btn_headers_6_management = WebElement(xpath='//*[@class = "tm-main-menu__item" and text()="Менеджмент"]')
+    btn_headers_7_marketing = WebElement(xpath='//*[@class = "tm-main-menu__item" and text()="Маркетинг"]')
+    btn_headers_8_popsci = WebElement(xpath='//*[@class = "tm-main-menu__item" and text()="Научпоп"]')
 
+    #footer buttons
+    btn_footer_1 = WebElement(xpath='//*[@class = "tm-footer-menu__block-title" and text()="Ваш аккаунт"]')
+    btn_footer_1_1 = WebElement(xpath='//li[@class = "tm-footer-menu__list-item"]/a[text()="Войти"]')
+    btn_footer_1_2 = WebElement(xpath='//li[@class = "tm-footer-menu__list-item"]/a[text()="Регистрация"]')
+
+    btn_footer_2 = WebElement(xpath='//*[@class = "tm-footer-menu__block-title" and text()="Разделы"]')
+    btn_footer_2_1 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Статьи"]')
+    btn_footer_2_2 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Новости"]')
+    btn_footer_2_3 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Хабы"]')
+    btn_footer_2_4 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Компании"]')
+    btn_footer_2_5 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Авторы"]')
+    btn_footer_2_6 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Песочница"]')
+
+    btn_footer_3 = WebElement(xpath='//*[@class = "tm-footer-menu__block-title" and text()="Информация"]')
+    btn_footer_3_1 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Устройство сайта"]')
+    btn_footer_3_2 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Для авторов"]')
+    btn_footer_3_3 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Для компаний"]')
+    btn_footer_3_4 = WebElement(xpath='//*[@class = "footer-menu__item-link" and text()="Документы"]')
+    btn_footer_3_5 = WebElement(xpath='//*[@class = "tm-footer-menu__list-item"]/a[text()="Соглашение"]')
+    btn_footer_3_6 = WebElement(xpath='//*[@class = "tm-footer-menu__list-item"]/a[text()="Конфиденциальность"]')
+
+    btn_footer_4 = WebElement(xpath='//*[@class = "tm-footer-menu__block-title" and text()="Услуги"]')
+    btn_footer_4_1 = WebElement(xpath='//*[@class = "tm-footer-menu__list-item"]/a[text()="Корпоративный блог"]')
+    btn_footer_4_2 = WebElement(xpath='//*[@class = "tm-footer-menu__list-item"]/a[text()="Медийная реклама"]')
+    btn_footer_4_3 = WebElement(xpath='//*[@class = "tm-footer-menu__list-item"]/a[text()="Нативные проекты"]')
+    btn_footer_4_4 = WebElement(xpath='//*[@class = "tm-footer-menu__list-item"]/a[text()="Образовательные программы"]')
+    btn_footer_4_5 = WebElement(xpath='//*[@class = "tm-footer-menu__list-item"]/a[text()="Стартапам"]')
--- page_test/tests/test_main_page.py
+++ page_test/tests/test_main_page.py
@@ -1,13 +1,155 @@
+import allure
 import pytest
 import pytest_check as check
-from Maksim_Tsybulka.class_work_9_f.page.locators import MainPage
+from page_test.page.locators import MainPage
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 
-
+@allure.story('Проверка главной страницы')
+@allure.feature('Проверка хедера')
 def test_headers(web_browser):
     """Этот тест проверяет хедер главной страницы"""
 
     page = MainPage(web_browser)
 
+    header_buttons = [
+            (page.btn_headers_1_my_feed, 'Моя лента'),
+            (page.btn_headers_2_all_streams, 'Все потоки'),
+            (page.btn_headers_3_development, 'Разработка'),
+            (page.btn_headers_4_admin, 'Администрирование'),
+            (page.btn_headers_5_design, 'Дизайн'),
+            (page.btn_headers_6_management, 'Менеджмент'),
+            (page.btn_headers_7_marketing, 'Маркетинг'),
+            (page.btn_headers_8_popsci, 'Научпоп')
+        ]
 
-    check.equal(page.btn_headers_domain.get_text(), 'Дом43ены', 'Тест локатора не равен ожидаймому результату')
-    check.is_true(page.btn_headers_mail.is_visible())
+    for button, button_text in header_buttons:
+        with allure.step(f'Проверка видимости элемента "{button_text}"'):
+            check.is_true(button.is_visible())
+
+        with allure.step(f'Проверка кликабельности элемента "{button_text}"'):
+            check.is_true(button.is_clickable())
+
+        with allure.step(f'Проверка текста элемента "{button_text}"'):
+            btn_text = button.get_text()
+            check.equal(btn_text, button_text, f'Тест локатора "{button_text}" не равен ожидаемому результату')
+
+#Тесты футера разбиты на разделы, т.к. проверка футера целиком выполняется очень долго
+@allure.story('Проверка главной страницы')
+@allure.feature('Проверка футера - раздел "Ваш аккаунт')
+def test_footer_account(web_browser):
+    """Этот тест проверяет раздел футера Ваш аккаунт"""
+
+    page = MainPage(web_browser)
+
+    footer_buttons = [
+            (page.btn_footer_1, 'Ваш аккаунт'),
+            (page.btn_footer_1_1, 'Войти'),
+            (page.btn_footer_1_2, 'Регистрация')
+        ]
+
+    page.scroll_down()
+    time.sleep(1)
+
+    for button, button_text in footer_buttons:
+        with allure.step(f'Проверка видимости элемента "{button_text}"'):
+            check.is_true(button.is_visible())
+
+        with allure.step(f'Проверка кликабельности элемента "{button_text}"'):
+            check.is_true(button.is_clickable())
+
+        with allure.step(f'Проверка текста элемента "{button_text}"'):
+            btn_text = button.get_text()
+            check.equal(btn_text, button_text, f'Тест локатора "{button_text}" не равен ожидаемому результату')
+
+@allure.story('Проверка главной страницы')
+@allure.feature('Проверка футера - раздел "Разделы')
+def test_footer_sections(web_browser):
+    """Этот тест проверяет раздел футера Разделы"""
+
+    page = MainPage(web_browser)
+
+    footer_buttons = [
+            (page.btn_footer_2, 'Разделы'),
+            (page.btn_footer_2_1, 'Статьи'),
+            (page.btn_footer_2_2, 'Новости'),
+            (page.btn_footer_2_3, 'Хабы'),
+            (page.btn_footer_2_4, 'Компании'),
+            (page.btn_footer_2_5, 'Авторы'),
+            (page.btn_footer_2_6, 'Песочница')
+        ]
+
+    page.scroll_down()
+    time.sleep(1)
+
+    for button, button_text in footer_buttons:
+        with allure.step(f'Проверка видимости элемента "{button_text}"'):
+            check.is_true(button.is_visible())
+
+        with allure.step(f'Проверка кликабельности элемента "{button_text}"'):
+            check.is_true(button.is_clickable())
+
+        with allure.step(f'Проверка текста элемента "{button_text}"'):
+            btn_text = button.get_text()
+            check.equal(btn_text, button_text, f'Тест локатора "{button_text}" не равен ожидаемому результату')
+
+@allure.story('Проверка главной страницы')
+@allure.feature('Проверка футера - раздел "Информация')
+def test_footer_information(web_browser):
+    """Этот тест проверяет раздел футера Информация"""
+
+    page = MainPage(web_browser)
+
+    footer_buttons = [
+            (page.btn_footer_3, 'Информация'),
+            (page.btn_footer_3_1, 'Устройство сайта'),
+            (page.btn_footer_3_2, 'Для авторов'),
+            (page.btn_footer_3_3, 'Для компаний'),
+            (page.btn_footer_3_4, 'Документы'),
+            (page.btn_footer_3_5, 'Соглашение'),
+            (page.btn_footer_3_6, 'Конфиденциальность')
+        ]
+
+    page.scroll_down()
+    time.sleep(1)
+
+    for button, button_text in footer_buttons:
+        with allure.step(f'Проверка видимости элемента "{button_text}"'):
+            check.is_true(button.is_visible())
+
+        with allure.step(f'Проверка кликабельности элемента "{button_text}"'):
+            check.is_true(button.is_clickable())
+
+        with allure.step(f'Проверка текста элемента "{button_text}"'):
+            btn_text = button.get_text()
+            check.equal(btn_text, button_text, f'Тест локатора "{button_text}" не равен ожидаемому результату')
+
+@allure.story('Проверка главной страницы')
+@allure.feature('Проверка футера - раздел "Услуги')
+def test_footer_services(web_browser):
+    """Этот тест проверяет раздел футера Услуги"""
+
+    page = MainPage(web_browser)
+
+    footer_buttons = [
+            (page.btn_footer_4, 'Услуги'),
+            (page.btn_footer_4_1, 'Корпоративный блог'),
+            (page.btn_footer_4_2, 'Медийная реклама'),
+            (page.btn_footer_4_3, 'Нативные проекты'),
+            (page.btn_footer_4_4, 'Образовательные программы'),
+            (page.btn_footer_4_5, 'Стартапам')
+        ]
+
+    page.scroll_down()
+    time.sleep(2)
+
+    for button, button_text in footer_buttons:
+        with allure.step(f'Проверка видимости элемента "{button_text}"'):
+            check.is_true(button.is_visible())
+
+        with allure.step(f'Проверка кликабельности элемента "{button_text}"'):
+            check.is_true(button.is_clickable())
+
+        with allure.step(f'Проверка текста элемента "{button_text}"'):
+            btn_text = button.get_text()
+            check.equal(btn_text, button_text, f'Тест локатора "{button_text}" не равен ожидаемому результату')
