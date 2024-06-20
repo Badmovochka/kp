77901ac0a327f6cad0704d71e364b5d537a7644e
Kristina P <badmovochka@gmail.com>
2024-06-20 20:19:45 +0300

    Checkboxes test created

---
+++ scriptsh/run_ui_tests.sh
@@ -0,0 +1,18 @@
+##!/bin/bash
+#cd ..
+#ls
+#pytest -v -s ./page_test/tests/test_main_page.py --alluredir=results
+
+# Получаем текущую директорию скрипта
+SCRIPT_DIR=$(dirname "$0")
+pwd
+
+# Определяем путь к тестовому файлу относительно директории со скриптом
+TEST_FILE="$SCRIPT_DIR/../page_test/tests"
+
+# Определяем путь к директории с отчетами относительно директории со скриптом
+REPORT_DIR="$SCRIPT_DIR/../results"
+
+# Запускаем pytest с использованием относительного пути к файлу test_my_project.py и директории с отчетами
+pytest -v -s "$TEST_FILE" --alluredir="$REPORT_DIR"
+
